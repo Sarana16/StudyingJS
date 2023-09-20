876a0d1fa7d32ddd94ea2898381776eb7e846a1f
Sarana16 <145531074+Sarana16@users.noreply.github.com>
2023-09-20 11:35:09 +0300

    lab#1
    по формуле из лабораторной работы создала код

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-let a = 5;
-condole.log(a);
+'use strict';
+
+let x = +prompt("введите x...");
+let y = +prompt("введите y...");
+
+let c = (1)/(2*Math.PI)-x*Math.sqrt(2.5*Math.pow(10,3)*y)*Math.abs(Math.cos(Math.pow(x,3)));
+
+console.log(c);
